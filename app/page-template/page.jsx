46349bec83a1b5bfcb7bd089125030cff5d62376
Manmeet <mnmtsnghh@gmail.com>
2023-07-03 18:40:50 +0530

    changes 101

--- app/page-template/page.jsx
+++ app/page-template/page.jsx
@@ -11,66 +11,7 @@ import ButtonContainer from "../components/ButtonContainer";
 import "../globals.css";
 
 const AnyComponentName = () => {
-  const [prompt, setPrompt] = useState("");
-  const [data, setData] = useState("");
-
-  const handlePromptChange = (e) => {
-    setPrompt(e.target.value);
-  };
-
-  const handleSubmit = async () => {
-    console.log(`sending ${prompt}`);
-    // STEP 1: Modify Endpoint
-    const response = await fetch("/api/", {
-      // STEP 2: Check Method
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ input: prompt }),
-    });
-
-    const searchRes = await response.json();
-    // Step 3: Double check the console log and setData accordingly
-    console.log(searchRes);
-    setData(searchRes.output);
-  };
-
-  return (
-    <>
-      <Title emoji="💬" headingText="PDF-GPT" />
-      <TwoColumnLayout
-        leftChildren={
-          <>
-            <PageHeader
-              heading="Catch Title"
-              boldText="Bold Text"
-              description="Description"
-            />
-            <ButtonContainer>
-              <Button
-                handleSubmit={handleSubmit}
-                endpoint="pdfupload-book"
-                buttonText="Upload"
-              />
-            </ButtonContainer>
-          </>
-        }
-        rightChildren={
-          <>
-            <ResultWithSources messages={messages} />
-            <PromptBox
-              prompt={prompt}
-              handlePromptChange={handlePromptChange}
-              handleSubmit={() => handleSubmitQuery("/endpoint")}
-              placeHolderText={"...."}
-              error={error}
-            />
-          </>
-        }
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default AnyComponentName;
