65bf00adacef198dbcb56acbcb6c9c6a8293e281
Manmeet <mnmtsnghh@gmail.com>
2023-07-04 19:21:45 +0530

    add multiple headings in PageHeader component

--- app/components/PageHeader.jsx
+++ app/components/PageHeader.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { pressStart2P, instrumentSans } from "../styles/fonts";
+import { pressStart2P, sourceCodePro, instrumentSans } from "../styles/fonts";
 
-const PageHeader = ({ heading, boldText, description }) => {
+const PageHeader = ({ heading1, heading2, boldText, description }) => {
   return (
     <>
-      <h1 className={`${pressStart2P.className} mb-10 text-6xl uppercase`}>
-        {heading}
+      <h1 className={`${pressStart2P.className} mb-3 text-4xl uppercase`}>
+        {heading1}
       </h1>
-      <p className={`${instrumentSans.className} mb-10`}>
+      <h2 className={`${sourceCodePro.className} mb-10 text-3xl uppercase`}>
+        {heading2}
+      </h2>
+
+      <p className={`${instrumentSans.className} text-xl mb-10`}>
         <strong>{boldText}</strong> {description}
       </p>
     </>
