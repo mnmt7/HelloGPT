37fdd482f32eeded7743110514997588b20bc0fc
Manmeet <mnmtsnghh@gmail.com>
2023-07-04 19:21:00 +0530

    update Homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,36 +2,34 @@
 import React from "react";
 import Image from "next/image";
 import { pressStart2P, sourceCodePro, instrumentSans } from "./styles/fonts";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-11/12 m-auto flex-col my-6">
-      <div className="rounded-3xl px-12 py-4 shadow-2xl">
-        <ul className="grid grid-cols-3 gap-4 uppercase place-items-center">
-          <li className="flex flex-col border-solid">
-            <a href="/pdf">
-              <div className="rounded-xl overflow-hidden h-[300px] w-[300px] drop-shadow">
-                <div className="absolute inset-0">
-                  <Image
-                    src="/assets/images/pdf.png"
-                    alt="robot reading newspaper"
-                    fill
-                    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
-                    style={{ objectFit: "cover" }}
-                    className="transform transition-transform hover:scale-105"
-                  />
-                </div>
+    <div className="w-11/12 m-auto my-6">
+      <div className="rounded-3xl px-8 py-4 shadow-2xl">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 uppercase place-content-center">
+          <li>
+            <Link href="/pdf">
+              <div className="rounded-xl overflow-hidden drop-shadow h-72 max-w-xs mx-auto">
+                <Image
+                  src="/assets/images/pdf.png"
+                  alt="robot reading newspaper"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="transform transition-transform hover:scale-105"
+                />
               </div>
               <p
-                className={`text-2xl font-bold text-black ${sourceCodePro.className}`}
+                className={`text-2xl text-center font-bold text-black ${sourceCodePro.className}`}
               >
                 PDF-GPT
               </p>
-            </a>
+            </Link>
           </li>
-          <li className="flex flex-col gap-4">
-            <a href="/memory">
-              <div className="rounded-xl overflow-hidden h-[300px] w-[300px] drop-shadow">
+          <li>
+            <Link href="/memory">
+              <div className="rounded-xl overflow-hidden drop-shadow h-72 max-w-xs mx-auto">
                 <div className="absolute inset-0">
                   <Image
                     src="/assets/images/brain.png"
@@ -44,15 +42,15 @@ export default function Home() {
                 </div>
               </div>
               <p
-                className={`text-2xl font-bold text-black ${sourceCodePro.className}`}
+                className={`text-2xl text-center font-bold text-black ${sourceCodePro.className}`}
               >
                 Memory
               </p>
-            </a>
+            </Link>
           </li>
-          <li className="flex flex-col gap-4">
-            <a href="streaming">
-              <div className="rounded-xl overflow-hidden h-[300px] w-[300px] drop-shadow">
+          <li>
+            <Link href="streaming">
+              <div className="rounded-xl overflow-hidden drop-shadow h-72 max-w-xs mx-auto">
                 <div className="absolute inset-0">
                   <Image
                     src="/assets/images/stream.png"
@@ -65,15 +63,15 @@ export default function Home() {
                 </div>
               </div>
               <p
-                className={`text-2xl font-bold text-black ${sourceCodePro.className}`}
+                className={`text-2xl text-center font-bold text-black ${sourceCodePro.className}`}
               >
                 Streaming
               </p>
-            </a>
+            </Link>
           </li>
-          <li className="flex flex-col gap-4">
-            <a href="video-chat">
-              <div className="rounded-xl overflow-hidden h-[300px] w-[300px] drop-shadow">
+          <li>
+            <Link href="video-chat">
+              <div className="rounded-xl overflow-hidden drop-shadow h-72 max-w-xs mx-auto">
                 <div className="absolute inset-0">
                   <Image
                     src="/assets/images/youtube.png"
@@ -86,15 +84,15 @@ export default function Home() {
                 </div>
               </div>
               <p
-                className={`text-2xl font-bold text-black ${sourceCodePro.className}`}
+                className={`text-2xl text-center font-bold text-black ${sourceCodePro.className}`}
               >
                 YT Vid Chat
               </p>
-            </a>
+            </Link>
           </li>
-          <li className="flex flex-col gap-4">
-            <a href="content-generator">
-              <div className="rounded-xl overflow-hidden h-[300px] w-[300px] drop-shadow">
+          <li>
+            <Link href="content-generator">
+              <div className="rounded-xl overflow-hidden drop-shadow h-72 max-w-xs mx-auto">
                 <div className="absolute inset-0">
                   <Image
                     src="/assets/images/wizard.png"
@@ -107,15 +105,15 @@ export default function Home() {
                 </div>
               </div>
               <p
-                className={`text-2xl font-bold text-black ${sourceCodePro.className}`}
+                className={`text-2xl text-center font-bold text-black ${sourceCodePro.className}`}
               >
                 Content Wiz
               </p>
-            </a>
+            </Link>
           </li>
-          <li className="flex flex-col gap-4">
-            <a href="resume-reader">
-              <div className="rounded-xl overflow-hidden h-[300px] w-[300px] drop-shadow">
+          <li>
+            <Link href="resume-reader">
+              <div className="rounded-xl overflow-hidden drop-shadow h-72 max-w-xs mx-auto">
                 <div className="absolute inset-0">
                   <Image
                     src="/assets/images/robohr.png"
@@ -128,11 +126,11 @@ export default function Home() {
                 </div>
               </div>
               <p
-                className={`text-2xl font-bold text-black ${sourceCodePro.className}`}
+                className={`text-2xl text-center font-bold text-black ${sourceCodePro.className}`}
               >
                 RoboHR
               </p>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
