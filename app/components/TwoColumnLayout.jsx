bdd19a70d84c9efbcaa9a62572c0bf5b2bb73b11
Manmeet <mnmtsnghh@gmail.com>
2023-07-05 19:31:21 +0530

    add gap between left and right child in TwoColumnLayout

--- app/components/TwoColumnLayout.jsx
+++ app/components/TwoColumnLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TwoColumnLayout = ({ leftChildren, rightChildren }) => (
-  <div className="flex flex-col justify-between  md:flex-row py-20">
+  <div className="flex flex-col justify-between  md:flex-row py-20 gap-10 md:gap-0">
     {/* Description */}
     <div className="md:w-2/5 w-full">{leftChildren}</div>
     {/* Chat */}
