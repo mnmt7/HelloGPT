8370c7b143f4a0c0a6f6465d461800fc522a1501
Manmeet <mnmtsnghh@gmail.com>
2023-07-05 17:07:18 +0530

    add upload button

--- app/pdf/page.jsx
+++ app/pdf/page.jsx
@@ -5,14 +5,49 @@ import ResultWithSources from "../components/ResultWithSources";
 import PromptBox from "../components/PromptBox";
 import Button from "../components/Button";
 import PageHeader from "../components/PageHeader";
-import Title from "../components/Title";
 import TwoColumnLayout from "../components/TwoColumnLayout";
 import ButtonContainer from "../components/ButtonContainer";
 import "../globals.css";
 
-// This functional component is responsible for loading PDFs
+// import loader from "./loader.svg";
+
+import { useDropzone } from "react-dropzone";
+
 const PDFLoader = () => {
-  // Managing prompt, messages, and error states with useState
+  const [uploading, setUploading] = useState(false);
+  const [generating, setGenerating] = useState(false);
+
+  const onDrop = async (acceptedFiles) => {
+    setUploading(true);
+
+    const file = acceptedFiles[0];
+
+    try {
+      const formData = new FormData();
+      formData.append("pdfFile", file);
+
+      const response = await fetch("/api/pdf-upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("File uploaded successfully");
+      } else {
+        console.error("Error uploading file");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: "application/pdf",
+  });
+
   const [prompt, setPrompt] = useState("How to get rich?");
   const [messages, setMessages] = useState([
     {
@@ -22,46 +57,38 @@ const PDFLoader = () => {
   ]);
   const [error, setError] = useState("");
 
-  // This function updates the prompt value when the user types in the prompt box
   const handlePromptChange = (e) => {
     setPrompt(e.target.value);
   };
 
-  // This function handles the submission of the form when the user hits 'Enter' or 'Submit'
-  // It sends a GET request to the provided endpoint with the current prompt as the query
   const handleSubmit = async (endpoint) => {
     try {
       console.log(`sending ${prompt}`);
       console.log(`using ${endpoint}`);
 
-      // A GET request is sent to the backend
       const response = await fetch(`/api/${endpoint}`, {
         method: "GET",
       });
 
-      // The response from the backend is parsed as JSON
       const searchRes = await response.json();
       console.log(searchRes);
-      setError(""); // Clear any existing error messages
+      setError("");
     } catch (error) {
       console.log(error);
       setError(error.message);
     }
   };
 
-  // This function handles the submission of the user's prompt when the user hits 'Enter' or 'Submit'
-  // It sends a POST request to the provided endpoint with the current prompt in the request body
   const handleSubmitPrompt = async (endpoint) => {
+    setGenerating(true);
     try {
       setPrompt("");
 
-      // Push the user's message into the messages array
       setMessages((prevMessages) => [
         ...prevMessages,
         { text: prompt, type: "user", sourceDocuments: null },
       ]);
 
-      // A POST request is sent to the backend with the current prompt in the request body
       const response = await fetch(`/api/${endpoint}`, {
         method: "POST",
         headers: {
@@ -70,17 +97,14 @@ const PDFLoader = () => {
         body: JSON.stringify({ input: prompt }),
       });
 
-      // Throw an error if the HTTP status is not OK
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      // Parse the response from the backend as JSON
       const searchRes = await response.json();
 
       console.log({ searchRes });
 
-      // Push the response into the messages array
       setMessages((prevMessages) => [
         ...prevMessages,
         {
@@ -90,54 +114,50 @@ const PDFLoader = () => {
         },
       ]);
 
-      setError(""); // Clear any existing error messages
+      setError("");
     } catch (error) {
       console.log(error);
       setError(error.message);
+    } finally {
+      setGenerating(false);
     }
   };
 
-  // The component returns a two column layout with various child components
   return (
     <>
-      <Title emoji="💬" headingText="PDF-GPT" />
       <TwoColumnLayout
         leftChildren={
           <>
             <PageHeader
-              heading="Ask Naval Anything"
-              boldText="How to get rich? How to be happy?"
+              heading1="💬 PDF-GPT"
+              heading2="Ask anything contained in a PDF document"
               description="This tool will
             let you ask anything contained in a PDF document. This tool uses
-            Embeddings, Pinecone, VectorDBQAChain, and VectorStoreAgents. Head over to Module 1 to
-            get started!"
+            Embeddings, Pinecone, VectorDBQAChain, and VectorStoreAgents. The Book THE ALMANACK OF NAVAL RAVIKANT is provided as a sample. Click on the button below to upload your own PDF."
             />
-            <ButtonContainer>
-              {/* <Button
-                handleSubmit={()=>{handleSubmit('pdfupload-book')}}
-                endpoint="pdfuploadtest"
-                buttonText="Upload Test Data ☁️"
-                className="Button"
-              /> */}
-              <Button
-                handleSubmit={handleSubmit}
-                endpoint="pdf-upload"
-                buttonText="Upload Book 📚"
-                className="Button"
-              />
-            </ButtonContainer>
+            <div className="py-5 w-50 text-xl bg-teal-400 text-center rounded-xl hover:cursor-pointer hover:bg-teal-300">
+              {uploading ? (
+                <img src={""} alt="Uploading..." />
+              ) : (
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  {isDragActive ? "Drop Book 📚" : "Upload Book 📚"}
+                </div>
+              )}
+            </div>
           </>
         }
         rightChildren={
           <>
             <ResultWithSources messages={messages} pngFile="pdf" />
             <PromptBox
+              generating={generating}
               prompt={prompt}
               handlePromptChange={handlePromptChange}
               handleSubmit={() => handleSubmitPrompt("/pdf-query")}
-              // handleSubmit={() => handleSubmitQuery("/pdfquery-agent")}
               placeHolderText={"How to get rich?"}
               error={error}
+              disableButton={generating}
             />
           </>
         }
