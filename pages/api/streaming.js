d9925dcd5bfa2741bad4bf9d54c7a30524af40ce
Manmeet <mnmtsnghh@gmail.com>
2023-07-06 10:41:27 +0530

    change initial prompt of the streaming page

--- pages/api/streaming.js
+++ pages/api/streaming.js
@@ -24,7 +24,9 @@ export default function handler(req, res) {
 
     const [creator, thing] = input.split(", ");
     // create the prompt
-    const prompt = `You are ${creator}. Create me a short rap about ${thing}.`;
+    const prompt = `You are ${creator}. Create me a short song or rap (depending on what you are) about ${thing}. \n\n Make sure to first mention who you are, a brief bio about you and what you are going to do. This is not part of the song. So you can mention it in a normal manner.
+    
+    `;
     console.log(prompt);
     // call frontend to backend
     llm.call(prompt).then(() => {
